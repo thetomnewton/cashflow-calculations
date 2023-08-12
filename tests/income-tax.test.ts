12abd1cd81f96eed2cf4cf22018b57858125d4eb
Tom Newton <newton.thomasjames@gmail.com>
2023-08-12 22:58:33 +0100

    stub tests

--- tests/income-tax.test.ts
+++ tests/income-tax.test.ts
@@ -143,43 +143,43 @@ describe('income tax', () => {
     expect(outputIncomeYear.tax.bands.higher_rate_eng.tax_paid).toEqual(7892)
   })
 
-  // test('additional rate salary gets taxed correctly', () => {
-  //   //
-  // })
+  test('additional rate salary gets taxed correctly', () => {
+    //
+  })
 
-  // test('self employment income taxed correctly', () => {
-  //   //
-  // })
+  test('self employment income taxed correctly', () => {
+    //
+  })
 
-  // test('dividend income taxed correctly', () => {
-  //   //
-  // })
+  test('dividend income taxed correctly', () => {
+    //
+  })
 
-  // test('earned income uses scottish rates correctly', () => {
-  //   //
-  // })
+  test('earned income uses scottish rates correctly', () => {
+    //
+  })
 
-  // test('self employed income uses scottish rates correctly', () => {
-  //   //
-  // })
+  test('self employed income uses scottish rates correctly', () => {
+    //
+  })
 
-  // test('dividend income uses scottish rates correctly', () => {
-  //   //
-  // })
+  test('dividend income uses scottish rates correctly', () => {
+    //
+  })
 
-  // test('taxable "other" income taxed correctly', () => {
-  //   //
-  // })
+  test('taxable "other" income taxed correctly', () => {
+    //
+  })
 
-  // test('non-taxable "other" income is not taxed', () => {
-  //   //
-  // })
+  test('non-taxable "other" income is not taxed', () => {
+    //
+  })
 
-  // test('pension income taxed correctly', () => {
-  //   //
-  // })
+  test('pension income taxed correctly', () => {
+    //
+  })
 
-  // test('savings income taxed correctly', () => {
-  //   //
-  // })
+  test('savings income taxed correctly', () => {
+    //
+  })
 })
---
+++ tests/national-insurance.ts
@@ -0,0 +1,29 @@
+describe('national insurance', () => {
+  test('salary below threshold no NI to pay', () => {
+    //
+  })
+
+  test('under 16 no NICs', () => {
+    //
+  })
+
+  test('over state pension no NICs', () => {
+    //
+  })
+
+  test('salary above LPL taxed via class1', () => {
+    //
+  })
+
+  test('salary above UPL taxed appropriately with class1', () => {
+    //
+  })
+
+  test('self employed below LPL no NICs', () => {
+    //
+  })
+
+  test('self-employed pays both class2 and class4', () => {
+    //
+  })
+})
