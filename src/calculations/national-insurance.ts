9f27b47c885bd0a7a20c35607a335a3bf82c9ab3
Tom Newton <newton.thomasjames@gmail.com>
2023-08-12 21:17:10 +0100

    NICs wip

--- src/calculations/national-insurance.ts
+++ src/calculations/national-insurance.ts
@@ -5,17 +5,21 @@ import {
   class1Rates,
   class2Tax,
 } from '../config/national-insurance'
+import dayjs from 'dayjs'
+import { getYearIndex } from './income-tax'
 
 let taxYear: string
 let cashflow: Cashflow
+let output: Output
 
 export function calcNICs(
   year: PlanningYear,
   baseCashflow: Cashflow,
-  output: Output
+  baseOutput: Output
 ) {
   taxYear = year.tax_year
   cashflow = baseCashflow
+  output = baseOutput
 
   cashflow.incomes
     .filter(({ type }) => Object.keys(incomeClasses).includes(type))
@@ -27,6 +31,18 @@ export function calcNICs(
 }
 
 function shouldPayNICsThisYear(person: Person) {
-  // eligible to pay NICs if between 16 and state pension age
+  const age = personAgeAtDate(
+    person,
+    output.years[getYearIndex(taxYear, output)].starts_at
+  )
+
+  // todo: Eligible to pay NICs if between 16 and state pension age
+
+  if (age < 16) return false
+
   return true
+}
+
+function personAgeAtDate(person: Person, date: string) {
+  return dayjs.utc(person.date_of_birth).diff(date, 'year')
 }
