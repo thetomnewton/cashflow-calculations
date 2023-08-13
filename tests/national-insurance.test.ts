982d27d337591b85978a0503c70ec523eff3e76a
Tom Newton <newton.thomasjames@gmail.com>
2023-08-13 10:44:36 +0100

    add test

---
+++ tests/national-insurance.test.ts
@@ -0,0 +1,67 @@
+import { v4 } from 'uuid'
+import { makeCashflow, makeIncome, makePerson } from '../src/factories'
+import { run } from '../src/calculations'
+
+describe('national insurance', () => {
+  test('salary below threshold has no NI to pay', () => {
+    const person = makePerson({ date_of_birth: '1980-01-01', sex: 'male' })
+    const salaryId = v4()
+    const cashflow = makeCashflow({
+      starts_at: '2023-08-13',
+      years: 3,
+      people: [person],
+      incomes: [
+        makeIncome({
+          id: salaryId,
+          type: 'employment',
+          people: [person],
+          values: [
+            {
+              value: 10000,
+              starts_at: '2023-08-13',
+              ends_at: '2026-08-13',
+              adjusted: true,
+              escalation: 'cpi',
+            },
+          ],
+        }),
+      ],
+    })
+
+    const out = run(cashflow)
+
+    expect(out.incomes[salaryId].years[0].tax.ni_paid.class1).toBe(0)
+  })
+
+  test('under 16 no NICs', () => {
+    //
+  })
+
+  test('over state pension no NICs', () => {
+    //
+  })
+
+  test('salary above LPL taxed via class1', () => {
+    //
+  })
+
+  test('salary above UPL taxed appropriately with class1', () => {
+    //
+  })
+
+  test('self employed below LPL no NICs', () => {
+    //
+  })
+
+  test('self-employed pays both class2 and class4', () => {
+    //
+  })
+
+  test('NIC thresholds project forward (nominal terms)', () => {
+    //
+  })
+
+  test('NIC thresholds project forward (real terms)', () => {
+    //
+  })
+})
