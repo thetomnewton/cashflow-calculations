84f6363413776b0748f9ae2fb495e2ccba754464
Tom Newton <newton.thomasjames@gmail.com>
2023-12-06 16:19:50 +0000

    Update withdrawals.test.ts

--- tests/withdrawals.test.ts
+++ tests/withdrawals.test.ts
@@ -73,11 +73,56 @@ describe('planned withdrawals', () => {
     })
   })
 
-  test(`ongoing withdrawal escalates correctly`, () => {})
+  test(`ongoing withdrawal escalates correctly`, () => {
+    const person = makePerson({ date_of_birth: '1950-09-04', sex: 'female' })
 
-  test(`multiple withdrawals from same account both apply`, () => {})
+    const cash = makeAccount({
+      category: 'cash',
+      owner_id: person.id,
+      valuations: [{ date: iso(), value: 175123 }],
+      growth_template: { type: 'flat', rate: { gross_rate: 0.05, charges: 0 } },
+      withdrawals: [
+        {
+          value: 12121,
+          starts_at: iso(),
+          ends_at: iso('2028-12-31'),
+          escalation: 'rpi',
+        },
+      ],
+    })
 
-  test(`can make withdrawals from DC pension`, () => {})
+    const cashflow = makeCashflow({
+      people: [person],
+      starts_at: iso('2023-12-31'),
+      years: 5,
+      accounts: [cash],
+      assumptions: { rpi: 0.035 },
+    })
+
+    const output = run(cashflow)
+
+    expect(output.accounts[cash.id].years[0]).toEqual({
+      start_value: 175123,
+      current_value: 175123 - 12121,
+      end_value: (175123 - 12121) * 1.05,
+      net_growth: 0.05,
+    })
+
+    expect(output.accounts[cash.id].years[1]).toEqual({
+      start_value: 171152.1, // (175123 - 12121) * 1.05
+      current_value: 158606.86, // 171152.1 - 12121 * 1.035 (roughly)
+      end_value: 166537.2, // 158606.86 * 1.05
+      net_growth: 0.05,
+    })
+  })
+
+  test(`multiple withdrawals from same account both apply`, () => {
+    //
+  })
+
+  test(`can make withdrawals from DC pension`, () => {
+    //
+  })
 
   // test withdrawal from a joint account by 1 person?
   // test withdrawals from other types of accounts e.g. ISA
