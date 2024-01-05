7f001f6f00bb98e42fe6a198c098a072af7c5de7
Tom Newton <newton.thomasjames@gmail.com>
2024-01-05 13:23:15 +0000

    Update defined-benefit-pensions.test.ts

--- tests/defined-benefit-pensions.test.ts
+++ tests/defined-benefit-pensions.test.ts
@@ -1,3 +1,4 @@
+import { round } from 'lodash'
 import { v4 } from 'uuid'
 import { run } from '../src/calculations'
 import { makeCashflow, makePerson } from '../src/factories'
@@ -16,7 +17,7 @@ describe('defined benefit pensions', () => {
       deferment_escalation_rate: 0,
       active_escalation_rate: 0,
       normal_retirement_age: 65,
-      starts_at: iso('2029-06-01'),
+      starts_at: iso('2029-06-01'), // 65th birthday
     }
 
     const cashflow = makeCashflow({
@@ -34,5 +35,47 @@ describe('defined benefit pensions', () => {
     // Expect income to be created in year 6
     const netValues = out.incomes[inc.id].years.map(year => year.net_value)
     expect(netValues).toEqual([0, 0, 0, 0, 0, 0, 10000, 10000])
+  })
+
+  test('deferred DB escalates correctly', () => {
+    const person = makePerson({ date_of_birth: '1964-06-01' })
+
+    const db: DeferredDBPension = {
+      id: v4(),
+      owner_id: person.id,
+      status: 'deferred',
+      annual_amount: 10000,
+      deferment_escalation_rate: 0.03,
+      active_escalation_rate: 0.05,
+      normal_retirement_age: 65,
+      starts_at: iso('2029-06-01'),
+    }
+
+    const cashflow = makeCashflow({
+      people: [person],
+      defined_benefits: [db],
+      starts_at: iso('2023-06-01'),
+      years: 8,
+    })
+
+    const out = run(cashflow)
+
+    const inc = cashflow.incomes.find(inc => inc.source_id === db.id)
+    if (!inc) throw new Error('missing income')
+
+    // Expect income to be created in year 6
+    const netValues = out.incomes[inc.id].years.map(year => year.net_value)
+    const year6value = 10000 * 1.03 ** 6
+    const year7value = year6value * 1.05
+    expect(netValues).toEqual([
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      round(year6value, 2),
+      round(year7value, 2),
+    ])
   })
 })
